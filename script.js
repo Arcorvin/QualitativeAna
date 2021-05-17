48ba63674fcdfd8d806f0d22e4d2b5ee800c97ba
Arcorvin <77901990+Arcorvin@users.noreply.github.com>
2021-05-16 23:36:17 -0400

    hw1

---
+++ script.js
@@ -0,0 +1,8 @@
+var promp1 = prompt("Hey what is your name");
+var promp2 = prompt("Hey what is your favorite color");
+
+var who = "My name is " + promp1 + " and my favorite color is " + promp2;
+
+
+document.querySelector(".name").innerHTML = who;
+document.querySelector(".color").innerHTML = col;
